51a2a38ad20aa808247f483926ec834458b05130
Berry de Vos <bdevos@gmail.com>
2022-08-29 17:41:11 +0200

    Cleanup postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,6 @@
-// const OpenProps = require('open-props')
-
-// require('open-props/src/extra')
-
-const postcssCustomMedia = require('postcss-custom-media')
-// const postcssJitProps = require('postcss-jit-props')
-
-const postcssJitProps = require('postcss-jit-props')
 const OpenProps = require('open-props')
+const postcssCustomMedia = require('postcss-custom-media')
+const postcssJitProps = require('postcss-jit-props')
 
 module.exports = {
   plugins: [postcssCustomMedia, postcssJitProps(OpenProps)],
