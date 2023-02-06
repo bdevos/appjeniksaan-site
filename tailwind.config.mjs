462e9052f3dfeddaa1cf25374681dfde26cd87be
Berry de Vos <bdevos@gmail.com>
2023-02-06 16:19:20 +0100

    Code styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -16,6 +16,18 @@ export default {
           'Noto Sans',
           'sans-serif',
         ],
+        mono: [
+          'Dank Mono',
+          'Operator Mono',
+          'Inconsolata',
+          'Fira Mono',
+          'ui-monospace',
+          'SF Mono',
+          'Monaco',
+          'Droid Sans Mono',
+          'Source Code Pro',
+          'monospace',
+        ],
       },
       gridTemplateColumns: {
         archive: '3ch 1fr',
