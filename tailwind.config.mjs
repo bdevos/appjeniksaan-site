a76c28329f4b52d235e67fbe51b17f22488d17a0
Berry de Vos <bdevos@gmail.com>
2023-02-06 15:17:40 +0100

    Initial setup with tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ['Londrina Solid', 'sans-serif'],
+        body: ['Poppins', 'sans-serif'],
+        sans: [
+          'system-ui',
+          '-apple-system',
+          'Segoe UI',
+          'Roboto',
+          'Ubuntu',
+          'Cantarell',
+          'Noto Sans',
+          'sans-serif',
+        ],
+      },
+    },
+  },
+  plugins: [],
+}
