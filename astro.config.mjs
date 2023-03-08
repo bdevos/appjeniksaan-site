b3ffe4d508419ccb1f6fb0f7ca0e8412eeafa110
Berry de Vos <bdevos@gmail.com>
2023-03-08 10:34:47 +0100

    test with exp. assets set to false

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    assets: false,
+  },
   output: 'server',
   adapter: vercel(),
   markdown: {
