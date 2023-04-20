2cd0491f6e66e0809083f16aaf1a166574651b29
Berry de Vos <bdevos@gmail.com>
2023-04-20 10:26:36 +0200

    Run output at edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/static'
+import vercel from '@astrojs/vercel/edge'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   adapter: vercel(),
   markdown: {
     shikiConfig: {
