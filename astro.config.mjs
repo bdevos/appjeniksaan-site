149ffb4e938e2e14f4bc524ae520f4ba2465f310
Berry de Vos <bdevos@gmail.com>
2023-04-21 07:08:03 +0200

    edge does not seem to work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/edge'
+import vercel from '@astrojs/vercel/serverless'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    analytics: false,
+  }),
   markdown: {
     shikiConfig: {
       theme: 'vitesse-dark',
