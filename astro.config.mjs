8a85206ba28fdcf97d9a50bb052054b01e4924d4
Berry de Vos <bdevos@gmail.com>
2023-05-11 15:23:16 +0200

    Move to static build on deno deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/static'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: vercel({
-    analytics: false,
-  }),
   markdown: {
     shikiConfig: {
       theme: 'vitesse-dark',
