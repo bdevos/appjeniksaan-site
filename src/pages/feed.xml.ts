dd22edce6977bdb527f9f3e58b6de59b6c10b01a
Berry de Vos <bdevos@gmail.com>
2023-05-12 16:10:56 +0200

    Moved linked url to enclosure

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -12,38 +12,28 @@ const parser = new MarkdownIt()
 const sortByDate = (a: LinkedOrPosts, b: LinkedOrPosts) =>
   a.data.pubDate.localeCompare(b.data.pubDate) * -1
 
-const parseLinkedCustomData = (
-  item: CollectionEntry<'linked'>,
-  url: string
-): string =>
-  `<link rel="alternate" type="text/html" href="${item.data.href}"/>
-  <link rel="related" type="text/html" href="${url}${item.collection}/${item.slug}"/>`
-
-const parsePostsCustomData = (
-  item: CollectionEntry<'posts'>,
-  url: string
-): string =>
-  `<link rel="alternate" type="text/html" href="${url}${item.collection}/${item.slug}"/>`
+const parseLinkedAsEnclosure = (item: CollectionEntry<'linked'>) => ({
+  length: item.data.href.length,
+  url: item.data.href,
+  type: 'text/html',
+})
 
 export const get = async (context: APIContext) => {
   const linked = getCollection('linked')
   const posts = getCollection('posts')
 
   const items = [...(await linked), ...(await posts)].sort(sortByDate)
-  const url = context.site?.toString() ?? ''
 
   return rss({
     title: site.title,
     description: site.description,
     site: context.site?.toString() ?? '',
     items: items.map((item) => ({
-      customData:
-        item.collection === 'linked'
-          ? parseLinkedCustomData(item, url)
-          : parsePostsCustomData(item, url),
       link: `${item.collection}/${item.slug}`,
       title: item.data.title,
       pubDate: new Date(item.data.pubDate),
+      enclosure:
+        item.collection === 'linked' ? parseLinkedAsEnclosure(item) : undefined,
       description:
         item.collection === 'posts' && item.data.description
           ? item.data.description
