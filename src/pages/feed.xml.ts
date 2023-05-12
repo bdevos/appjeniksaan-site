564cd366e44e1dc3df784231faf7b232bad3be5b
Berry de Vos <bdevos@gmail.com>
2023-05-12 14:37:55 +0200

    Because of the way Atom's RSS generator works, had to do small fix

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -12,14 +12,25 @@ const parser = new MarkdownIt()
 const sortByDate = (a: LinkedOrPosts, b: LinkedOrPosts) =>
   a.data.pubDate.localeCompare(b.data.pubDate) * -1
 
-const parseLinkedCustomData = (item: CollectionEntry<'linked'>): string =>
-  `<link rel="alternate" type="text/html" href="${item.data.href}"/>`
+const parseLinkedCustomData = (
+  item: CollectionEntry<'linked'>,
+  url: string
+): string =>
+  `<link rel="alternate" type="text/html" href="${item.data.href}"/>
+  <link rel="related" type="text/html" href="${url}${item.collection}/${item.slug}"/>`
+
+const parsePostsCustomData = (
+  item: CollectionEntry<'posts'>,
+  url: string
+): string =>
+  `<link rel="alternate" type="text/html" href="${url}${item.collection}/${item.slug}"/>`
 
 export const get = async (context: APIContext) => {
   const linked = getCollection('linked')
   const posts = getCollection('posts')
 
   const items = [...(await linked), ...(await posts)].sort(sortByDate)
+  const url = context.site?.toString() ?? ''
 
   return rss({
     title: site.title,
@@ -27,7 +38,9 @@ export const get = async (context: APIContext) => {
     site: context.site?.toString() ?? '',
     items: items.map((item) => ({
       customData:
-        item.collection === 'linked' ? parseLinkedCustomData(item) : undefined,
+        item.collection === 'linked'
+          ? parseLinkedCustomData(item, url)
+          : parsePostsCustomData(item, url),
       link: `${item.collection}/${item.slug}`,
       title: item.data.title,
       pubDate: new Date(item.data.pubDate),
